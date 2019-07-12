b8401b68b5cf7bce666b1ea8f327871f1ce89927
Andydorsty <andrewsacheampong1@gmail.com>
2019-07-12 17:05:17 +0100

    Raspberry pie manipulations

---
+++ buzzer.py
@@ -0,0 +1,18 @@
+import time
+import RPi.GPIO as gpio
+
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(7,gpio.OUT)
+
+try:
+    
+    while True:
+        gpio.output(7,0)
+        time.sleep(.2)
+        gpio.output(7,1)
+        time.sleep(.2)
+        
+except keyboardInterrupt:
+    gpio.cleanup()
+    exit
